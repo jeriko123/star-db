bfe30f1b9069a7ad2df7ac06d2b3cda3181d48db
Yaroslav Kaniev <y.kaniev@gmail.com>
2019-10-20 16:05:12 +0300

    Updates

---
+++ precache-manifest.f56cfb4825fef3ddd304fa66f49803e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd3caa8a05b4e9dbfef6fb2eedfa0ba",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "9a14be69cf5d8113d257",
+    "url": "/star-db/static/css/main.6d25d506.chunk.css"
+  },
+  {
+    "revision": "6bce749604548e3cce97",
+    "url": "/star-db/static/js/2.f2de40a6.chunk.js"
+  },
+  {
+    "revision": "9a14be69cf5d8113d257",
+    "url": "/star-db/static/js/main.87e9a0fa.chunk.js"
+  },
+  {
+    "revision": "ed5d1bbf11aa36b8ad39",
+    "url": "/star-db/static/js/runtime~main.e24185d8.js"
+  }
+]);
